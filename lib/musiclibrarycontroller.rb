dcf74c3695c48b40ffe65ac3ca2cc4d6a27d1652
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-16 11:48:59 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -80,7 +80,9 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.strip.to_i - 1
     list_songs
-    p sorted = Song.all.sort_by { |song| song.name }[input - 1]
+    if valid?(input)
+    sorted = Song.all.sort_by { |song| song.name }[input - 1]
+    end
   end
 end
 

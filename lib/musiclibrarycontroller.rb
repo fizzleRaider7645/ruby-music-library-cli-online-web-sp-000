528c8ffae7f3414d4c8d006bd88dd9621d8d13ac
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-16 12:26:55 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -81,7 +81,6 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     list_songs
     input = gets.strip.to_i
-    p song = Song.all[input].name
     if input > 0 && input <= Song.all.length - 1
       
     else

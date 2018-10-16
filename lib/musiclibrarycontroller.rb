d7e89e5b2556cb19626a82d504d24aa45d2f5450
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-16 12:48:07 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -50,8 +50,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
     while (user_input = gets.chomp.downcase) != "exit"
-      case
-      when user_input == "list songs"
+      if user_input == "list songs"
         list_songs
       when user_input == "list artists"
         list_artists

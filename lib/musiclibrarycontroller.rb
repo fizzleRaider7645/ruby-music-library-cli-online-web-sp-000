e0f31f839a933c81b9315a1e64b65b4dcf7f1778
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-16 12:51:09 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -18,7 +18,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    until (input = gets.strip) != "exit"
+    until (input = gets.strip) == "exit"
       if input == "list songs"
         list_songs
       elsif input == "list artists"

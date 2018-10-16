de227cd94c210b982f27e45cee4ba7d36fb828df
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-16 12:26:12 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
     list_songs
     input = gets.strip.to_i
     if input > 0 && input <= Song.all.length - 1
-      song = Song.all[input].name
+      p song = Song.all[input].name
       
     else
       return
